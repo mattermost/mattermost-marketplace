29c44ddc3616622db084badd67ce753d8413c4a6
Ali Farooq <ali.farooq0@pm.me>
2019-11-09 14:39:50 -0500

    Linted

--- internal/store/plugin_test.go
+++ internal/store/plugin_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 func TestPlugins(t *testing.T) {
-	demoPlugin_V1Min514 := &model.Plugin{
+	demoPluginV1Min514 := &model.Plugin{
 		HomepageURL: "https://github.com/mattermost/mattermost-plugin-demo",
 		IconData:    "icon-data.svg",
 		DownloadURL: "https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.1.0/com.mattermost.demo-plugin-0.1.0.tar.gz",
@@ -26,7 +26,7 @@ func TestPlugins(t *testing.T) {
 		Signatures: []*model.PluginSignature{{Signature: "signature1", PublicKeyHash: "hash1"}},
 	}
 
-	demoPlugin_V2Min515 := &model.Plugin{
+	demoPluginV2Min515 := &model.Plugin{
 		HomepageURL: "https://github.com/mattermost/mattermost-plugin-demo",
 		IconData:    "icon-data.svg",
 		DownloadURL: "https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.2.0/com.mattermost.demo-plugin-0.2.0.tar.gz",
@@ -40,7 +40,7 @@ func TestPlugins(t *testing.T) {
 		Signatures: []*model.PluginSignature{{Signature: "signature1", PublicKeyHash: "hash1"}},
 	}
 
-	starterPlugin_V1Min515 := &model.Plugin{
+	starterPluginV1Min515 := &model.Plugin{
 		HomepageURL: "https://github.com/mattermost/mattermost-plugin-starter-template",
 		IconData:    "icon-data2.svg",
 		DownloadURL: "https://github.com/mattermost/mattermost-plugin-starter-template/releases/download/v0.1.0/com.mattermost.plugin-starter-template-0.1.0.tar.gz",
@@ -55,9 +55,9 @@ func TestPlugins(t *testing.T) {
 	}
 
 	data, err := json.Marshal([]*model.Plugin{
-		demoPlugin_V1Min514,
-		demoPlugin_V2Min515,
-		starterPlugin_V1Min515,
+		demoPluginV1Min514,
+		demoPluginV2Min515,
+		starterPluginV1Min515,
 	})
 	require.NoError(t, err)
 
@@ -82,7 +82,7 @@ func TestPlugins(t *testing.T) {
 			Filter:  "",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515}, actualPlugins)
 	})
 
 	t.Run("page 0, per page 10", func(t *testing.T) {
@@ -92,7 +92,7 @@ func TestPlugins(t *testing.T) {
 			Filter:  "",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515, starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515, starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("page 0, per page 1", func(t *testing.T) {
@@ -102,7 +102,7 @@ func TestPlugins(t *testing.T) {
 			Filter:  "",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515}, actualPlugins)
 	})
 
 	t.Run("page 0, per page 10", func(t *testing.T) {
@@ -112,7 +112,7 @@ func TestPlugins(t *testing.T) {
 			Filter:  "",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515, starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515, starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("default paging", func(t *testing.T) {
@@ -120,7 +120,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515, starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515, starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("filter spaces", func(t *testing.T) {
@@ -128,7 +128,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "  ",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515, starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515, starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("id match, exact", func(t *testing.T) {
@@ -136,7 +136,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "com.mattermost.demo-plugin",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515}, actualPlugins)
 	})
 
 	t.Run("id match, case-insensitive", func(t *testing.T) {
@@ -144,7 +144,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "com.mattermost.demo-PLUGIN",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515}, actualPlugins)
 	})
 
 	t.Run("name match, exact", func(t *testing.T) {
@@ -152,7 +152,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "Plugin Starter Template",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("name match, partial", func(t *testing.T) {
@@ -160,7 +160,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "Starter",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("name match, case-insensitive", func(t *testing.T) {
@@ -168,7 +168,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "TEMPLATE",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("description match, partial", func(t *testing.T) {
@@ -176,7 +176,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "capabilities",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515}, actualPlugins)
 	})
 
 	t.Run("description match, case-insensitive, multiple matches", func(t *testing.T) {
@@ -184,7 +184,7 @@ func TestPlugins(t *testing.T) {
 			Filter: "MATTERMOST",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515, starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515, starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("plugins that satisfy 5.15", func(t *testing.T) {
@@ -193,7 +193,7 @@ func TestPlugins(t *testing.T) {
 			ServerVersion: "5.15.0",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V2Min515, starterPlugin_V1Min515}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV2Min515, starterPluginV1Min515}, actualPlugins)
 	})
 
 	t.Run("plugins that satisfy 5.14", func(t *testing.T) {
@@ -202,7 +202,7 @@ func TestPlugins(t *testing.T) {
 			ServerVersion: "5.14.0",
 		})
 		require.NoError(t, err)
-		require.Equal(t, []*model.Plugin{demoPlugin_V1Min514}, actualPlugins)
+		require.Equal(t, []*model.Plugin{demoPluginV1Min514}, actualPlugins)
 	})
 
 	t.Run("with a server version that does not satisfy any plugin", func(t *testing.T) {
