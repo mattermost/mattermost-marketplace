9bd118832d75318dab124c52273ed0098e4699b8
Ben Schumacher <ben.schumacher@mattermost.com>
2020-01-10 11:14:37 +0100

    Add color field

--- internal/model/label.go
+++ internal/model/label.go
@@ -5,10 +5,12 @@ type Label struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	URL         string `json:"url"`
+	Color       string `json:"color"`
 }
 
 var OfficialLabel Label = Label{
 	Name:        "Official",
 	Description: "This plugin is maintained by Mattermost",
 	URL:         "https://mattermost.com/pl/default-community-plugins",
+	Color:       "#166de0",
 }
