5f9e5fc2a67a98d028d7a7b4abb77c769b0ae56e
Ben Schumacher <ben.schumacher@mattermost.com>
2020-01-21 10:52:33 +0100

    Fix spelling mistakes

--- cmd/generator/add.go
+++ cmd/generator/add.go
@@ -23,8 +23,8 @@ var addCmd = &cobra.Command{
 	Use:   "add [repo] [tag]",
 	Short: "Add a plugin release to the plugins.json database",
 	Long: "The generator commands allows adding a specific plugin release to the database by using this command.\n\n" +
-		"The release has to be build first using the cut plugin command, which also uploads it to https://plugins-store.test.mattermost.com/release/. " +
-		"This localtion is used to fetch the plugin release.",
+		"The release has to be built first using the /mb cutPlugin command, which also uploads it to https://plugins-store.test.mattermost.com/release/. " +
+		"This location is used to fetch the plugin release.",
 	Example: `  generator add matterpoll v1.5.1`,
 	Args:    cobra.ExactArgs(2),
 	RunE: func(command *cobra.Command, args []string) error {
