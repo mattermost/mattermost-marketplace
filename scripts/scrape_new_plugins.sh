267ef4e3569ee16365c542922f3cb96a457330e6
mickmister <6913320+mickmister@users.noreply.github.com>
2024-04-17 14:39:19 -0400

    implement script to scrape marketplace repo for new releases, based off of existing actions workflow
    changes:
    * fetch plugins.json through GitHub's raw file URL
    * create webhook post at the end of the script

--- scripts/scrape_new_plugins.sh
+++ scripts/scrape_new_plugins.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
-CHANGES=$(git diff HEAD~1 plugins.json)
-if [ -z "$CHANGES" ]
+if [ -z "$MM_WEBHOOK_URL" ]
 then
-  echo "no changes to plugins.json"
-  echo "{}" > mattermost.json
+  echo "Please set MM_WEBHOOK_URL environment variable"
   exit 0
 fi
 
-JSON=$(git diff HEAD~1 plugins.json | grep '^+ ' | sed 's/+//g' | sed '$s/},/}/g')
+LOCAL_PLUGINSJSON_PATH="plugins.json"
+REMOTE_PLUGINSJSON_PATH="new_plugins.json"
+
+curl https://raw.githubusercontent.com/mattermost/mattermost-marketplace/production/plugins.json -o $REMOTE_PLUGINSJSON_PATH
+
+CHANGES="$(git diff --no-index $LOCAL_PLUGINSJSON_PATH $REMOTE_PLUGINSJSON_PATH)"
+
+# Save the original plugins.json as a backup
+mv $LOCAL_PLUGINSJSON_PATH ${LOCAL_PLUGINSJSON_PATH}.bak
+mv $REMOTE_PLUGINSJSON_PATH $LOCAL_PLUGINSJSON_PATH
+
+if [ -z "$CHANGES" ]
+then
+  echo "no changes to plugins.json"
+  exit 0
+fi
+
+JSON="$(echo "$CHANGES" | grep '^+ ' | sed 's/^+ //g' | sed '$s/},/}/g')"
+
 NAME=$(jq -r .manifest.name <<< "$JSON")
 VERSION=$(jq -r .manifest.version <<< "$JSON")
 MIN_SERVER_VERSION=$(jq -r .manifest.min_server_version <<< "$JSON")
@@ -24,4 +39,6 @@ echo '{
     "text": "Release notes can be found [here]('$RELEASE_NOTES'). It requires Mattermost Server '$MIN_SERVER_VERSION'."
   }]
 }' > mattermost.json
+
+curl -i -X POST -H 'Content-Type: application/json' -d @mattermost.json $MM_WEBHOOK_URL
 cat mattermost.json
